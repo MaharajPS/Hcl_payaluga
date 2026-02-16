750604894ef91defbea47cd7e1bed6d00d1bbc56
Developer <dev@example.com>
2026-02-16 14:08:54 +0530

    body page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import CustomNavbar from "./components/CustomNavbar";
 import Footer from "./components/Footer";
+import Body from "./components/Body"
 
 function App() {
   return (
     <>
       <CustomNavbar />
       <div style={{ paddingTop: "90px", textAlign: "center" }}>
-        <h1>Welcome to My Website</h1>
+        <Body/>
       </div>
       <Footer/>
     </>
